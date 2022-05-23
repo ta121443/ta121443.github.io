4c463d28f21f8340f8478639b7bcce1342d74641
ta121443 <ryo_uchi4_20@outlook.jp>
2022-05-24 00:18:59 +0900

    update todo.html

--- todo/todo.js
+++ todo/todo.js
@@ -1,31 +1,60 @@
-const $listButton = document.querySelectorAll('.listButton');
-const $addButton = document.getElementById('addButton');
+const $form = document.getElementById('form');
+const $inputButton = document.getElementById('inputButton');
+const $input = document.getElementById('input');
+const $ul = document.getElementById('ul');
+const todos = JSON.parse(localStorage.getItem("todos"));
 
-$listButton.forEach((button) =>{
-    button.addEventListener('click', () => {
-        const li = button.closest('li');
-        li.classList.add('done');    
-    })
+if(todos) {
+    todos.forEach(todo => {
+        add(todo);
+    });
+}
+$inputButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log($input.value);
+    add();
 })
 
-$addButton.addEventListener('click', () => {
-    const input = document.getElementById('newTodo');
+function add(todo) {    //入力された値をTodoリストに追加して表示する
+    let todoText = $input.value;
+    if(todo) {
+        todoText = todo.text;
+    };
+    if(todoText){
+        const li = document.createElement("li");
+        li.innerText = todoText;
+        li.classList.add("list-item");
 
-    const $li = document.createElement('li');
-    $li.innerText = input.value;
-    const doneButton = document.createElement('button');
-    doneButton.innerText = 'done';
-    $li.appendChild(doneButton);
-    doneButton.addEventListener('click', () => {
-        $li.classList.add('done');
-    })
+        if(todo && todo.completed) {
+            li.classList.add("done");
+        };
 
-    const $ul = document.getElementById('list');
-    $ul.appendChild($li);
+        li.addEventListener("contextmenu", function(event) {
+            event.preventDefault();
+            li.remove();
+            saveData();
+        });
 
-})
+        li.addEventListener("click", function() {
+            li.classList.toggle("done");    //引数のクラスを持っていれば削除、持っていなければ追加
+            saveData();
+        });
 
+        $ul.appendChild(li);
+        $input.value = "";
+        saveData();
+    };
+}
 
-/*$button.addEventListener('click', () => {
-    console.log('clickbutton');
-})*/
+function saveData() {   //追加した情報を配列に保存する
+    const lists = document.querySelectorAll("li");
+    let todos = [];
+    lists.forEach(list => {
+        let todo = {
+            text: list.innerText,
+            completed: list.classList.contains("done")
+        }
+        todos.push(todo);
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));   //JSON.stringify()でjsonファイルに変換、扱いやすくする
+}
